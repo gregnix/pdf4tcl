6c0cb776c691031db760957d250758932d448f3e
Peter Spjuth <peter.spjuth@gmail.com>
2016-08-28 00:27:37 +0200

    Documented XObject -noimage

--- examples/test1.tcl
+++ examples/test1.tcl
@@ -3,6 +3,9 @@
 lappend auto_path [pwd]/../..
 package require pdf4tcl
 
+pdf4tcl::loadBaseTrueTypeFont BaseArial "../examples/FreeSans.ttf"
+pdf4tcl::createFont BaseArial MyArial1 iso8859-1
+
 pdf4tcl::new p1 -compress false -paper a4
 # Use an XObject as appeareance for a form below
 set xobj1 [p1 startXObject]
@@ -14,6 +17,14 @@ p1 setFillColor \#FF0000
 p1 setStrokeColor \#0000FF
 p1 circle 50 50 40 -filled 1
 p1 circle 50 50 20
+p1 endPage
+set xobj3 [p1 startXObject]
+p1 setFont 12 "MyArial1"
+p1 text "StampStamp" -x 5 -y 50
+p1 endPage
+set xobj4 [p1 startXObject -noimage 1]
+p1 setFont 12 "MyArial1"
+p1 text "NoImage" -x 5 -y 50
 p1 endPage
 
 p1 startPage
@@ -62,10 +73,18 @@ p1 setFillColor 0 0 0
 p1 drawTextBox 350 410 220 50 "Eine links- oder rechtsbündige und auch eine zentrierte Ausrichtung in der Textbox sind ebenfalls möglich." -align right
 p1 addImage tcl.jpg -id 1
 p1 putImage 1 20 20 -height 75
-p1 rectangle 95 95 20 20
-p1 addForm checkbutton 95 95 20 20 -on $xobj1
-p1 rectangle 115 115 20 20
-p1 addForm checkbutton 115 115 20 20 -on $xobj2
+
+p1 rectangle 395 495 20 20
+p1 addForm checkbutton 395 495 20 20 -on $xobj1
+p1 rectangle 415 515 20 20
+p1 addForm checkbutton 415 515 20 20 -on $xobj2
+p1 rectangle 435 535 20 20
+p1 addForm checkbutton 435 535 20 20 -on $xobj3
+p1 rectangle 455 535 20 20
+p1 addForm checkbutton 455 535 20 20 -on $xobj4
+p1 putImage $xobj2 460 560 -anchor nw -width 25
+p1 putImage $xobj3 490 590 -anchor nw -width 25
+#p1 putImage $xobj4 490 620 -anchor nw -width 25
 p1 write -file test1.pdf
 p1 destroy
 
