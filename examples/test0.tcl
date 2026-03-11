2fa0d30e9c8531f2b5f1e4447a3d54390f174fc5
gregnix <gregor.ebbing@gmx.de>
2026-03-11 03:59:00 +0100

    0.9.4.8: PDF/A-1b/2b support + /Length fix

--- examples/test0.tcl
+++ examples/test0.tcl
@@ -1,18 +1,30 @@
 #!/usr/bin/env tclsh
 
+# ./pdf4tcl/examples/test0.tcl
 set auto_path [linsert $auto_path 0 [file normalize [file join [file dirname [info script]] ..]]]
 
 package require pdf4tcl
 
 set ver  [package present pdf4tcl]
 set file [lindex [package ifneeded pdf4tcl $ver] ]
+set scriptfile [info script]
 set scriptdir [file dirname [info script]]
+set pwddir [pwd]
 puts "pdf4tcl $ver load froms: \n$file"
 puts "scriptdir: \n$scriptdir"
+puts "scriptfile:_\n$scriptfile"
+puts "pwd: \n$pwddir\n"
 puts "\nauto_path:\n[join $auto_path \n]\n"
 
 
 pdf4tcl::new p1 -compress 0
+
+p1 metadata \
+        -title    "pdf4tcl [file tail $scriptfile]" \
+        -keywords  "./pdf4tcl/examples/test0.tcl" \
+        -creator  "pdf4tcl $ver" \
+        -moddate  [clock seconds]
+
 p1 startPage -paper {595 842}
 p1 write -file test0.pdf
 p1 destroy
