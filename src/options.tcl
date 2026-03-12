f50e8884e7422837ed17e4e47f92ddd5d7b10d51
gregnix <gregor.ebbing@gmx.de>
2026-03-12 10:25:36 +0100

    0.9.4.10: setAlpha/getAlpha transparency, fix throw error codes, licence.terms encoding, check90 target

--- src/options.tcl
+++ src/options.tcl
@@ -31,17 +31,17 @@ oo::define ::pdf4tcl::options {
         my variable optiondefs
         my variable optiondeflist
         if {[llength $lst] % 2 != 0} {
-            throw "PDF4TCL" "wrong number of args"
+            throw {PDF4TCL} "wrong number of args"
         }
         foreach {option value} $lst {
             # TODO: recode to use prefix matching
             #tcl::prefix match $optiondeflist $option
             if {![dict exists $optiondefs $option]} {
-                throw "PDF4TCL" "unknown option \"$option\""
+                throw {PDF4TCL} "unknown option \"$option\""
             }
             if {[dict get $optiondefs $option -readonly] && \
                         [dict get $optiondefs $option _Initialised]} {
-                throw "PDF4TCL" \
+                throw {PDF4TCL} \
                         "option $option can only be set at instance creation"
             }
             if {[dict get $optiondefs $option -validatemethod] ne ""} {
@@ -90,14 +90,14 @@ oo::define ::pdf4tcl::options {
     method CheckPaper {option value} {
         set papersize [pdf4tcl::getPaperSize $value]
         if {[llength $papersize] == 0} {
-            throw "PDF4TCL" "papersize \"$value\" is unknown"
+            throw {PDF4TCL} "papersize \"$value\" is unknown"
         }
     }
 
     # Validator for -unit
     method CheckUnit {option value} {
         if {![info exists ::pdf4tcl::units($value)]} {
-            throw "PDF4TCL" "unit \"$value\" is unknown"
+            throw {PDF4TCL} "unit \"$value\" is unknown"
         }
     }
 
@@ -107,26 +107,26 @@ oo::define ::pdf4tcl::options {
             1 - 2 - 4 {
                 foreach elem $value {
                     if {[catch {pdf4tcl::getPoints $elem}]} {
-                        throw "PDF4TCL" "bad margin value \"$elem\""
+                        throw {PDF4TCL} "bad margin value \"$elem\""
                     }
                 }
             }
             default {
-                throw "PDF4TCL" "bad margin list \"$value\""
+                throw {PDF4TCL} "bad margin list \"$value\""
             }
         }
     }
     # Validator for boolean options
     method CheckBoolean {option value} {
         if {![string is boolean -strict $value]} {
-            throw "PDF4TCL" "option $option must have a boolean value"
+            throw {PDF4TCL} "option $option must have a boolean value"
         }
     }
 
     # Validator for word restricted options
     method CheckWord {option value} {
         if {![string is wordchar -strict $value]} {
-            throw "PDF4TCL" "option $option must be alphanumeric"
+            throw {PDF4TCL} "option $option must be alphanumeric"
         }
     }
 
@@ -134,14 +134,14 @@ oo::define ::pdf4tcl::options {
     method CheckRotation {option value} {
         my CheckNumeric $value rotation -nonnegative -integer
         if { $value % 90  } {
-            throw "PDF4TCL" "rotation $value not a multiple of 90"
+            throw {PDF4TCL} "rotation $value not a multiple of 90"
         }
     }
 
     # Validator for -pdfa: accepts "", "1b", "2b"
     method CheckPdfa {option value} {
         if {$value ne "" && $value ne "1b" && $value ne "2b"} {
-            throw "PDF4TCL" \
+            throw {PDF4TCL} \
                 "invalid -pdfa value \"$value\": must be \"\", \"1b\", or \"2b\""
         }
     }
@@ -158,24 +158,24 @@ oo::define ::pdf4tcl::options {
         if {$i >= 0} {
             set unit [lindex $args [expr {$i + 1}]]
             if {[catch {pdf4tcl::getPoints $val $unit} p]} {
-                throw "PDF4TCL" "bad $what \"$val\", must be numeric"
+                throw {PDF4TCL} "bad $what \"$val\", must be numeric"
             }
             set val $p
         }
         if {![string is double -strict $val]} {
-            throw "PDF4TCL" "bad $what \"$origVal\", must be numeric"
+            throw {PDF4TCL} "bad $what \"$origVal\", must be numeric"
         }
         set nonneg [lsearch -exact $args -nonnegative]
         set pos    [lsearch -exact $args -positive]
         set int    [lsearch -exact $args -integer]
         if {$nonneg >= 0 && $val < 0} {
-            throw "PDF4TCL" "bad $what \"$origVal\", may not be negative"
+            throw {PDF4TCL} "bad $what \"$origVal\", may not be negative"
         }
         if {$pos >= 0 && $val <= 0} {
-            throw "PDF4TCL" "bad $what \"$origVal\", must be positive"
+            throw {PDF4TCL} "bad $what \"$origVal\", must be positive"
         }
         if {$int >= 0 && ![string is integer -strict $val]} {
-            throw "PDF4TCL" "bad $what \"$origVal\", must be integer"
+            throw {PDF4TCL} "bad $what \"$origVal\", must be integer"
         }
         return $val
     }
